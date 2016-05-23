e789eed53e5becb255a91427339965dbe1733c61
Chris Matheson <chris@matheson.it>
2016-05-23 20:46:19 +0100

    start of CLI program

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const get = require('./get');
+const process = require('./extract_static_data');
 
 const URL = 'http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html';
 
-module.exports = function () {
-  return get(URL);
-};
+get(URL).then(process).then(console.log);
