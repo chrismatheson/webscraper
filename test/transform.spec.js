e2a2c55ece6bb109484cdf3f4c2ab0b4bbb6913f
Chris Matheson <chris@matheson.it>
2016-05-23 22:21:31 +0100

    add some simple happy path tests for formatting functions

---
+++ test/transform.spec.js
@@ -0,0 +1,42 @@
+const expect = require('expect.js');
+const transform = require('../transform');
+
+describe('transform', function () {
+
+  describe('parsePrice', function () {
+    it('should deal with weird octal edge cases', function () {
+      expect(transform.parsePrice('08')).to.be(8);
+    });
+  });
+
+  describe('trimWhiteSpace', function () {
+    it('should ignore all the rubbish HTML can spit out', function () {
+      const cleaned = transform.trimWhiteSpace('\n\t                                    \n\t                                        Sainsbury\'s Apricot Ripe & Ready x5\n\t                                        \n\t                                    \n\t                                ');
+      expect(cleaned).to.be('Sainsbury\'s Apricot Ripe & Ready x5');
+    });
+
+
+    it('should leave spaces between words', function () {
+      const cleaned = transform.trimWhiteSpace('\n\t                                    \n\t                                        Sainsbury\'s Apricot Ripe & Ready x5\n\t                                        \n\t                                    \n\t                                ');
+      expect(cleaned).to.match(/ /);
+    });
+
+    it('should not modify when not required', function () {
+      const cleaned = transform.trimWhiteSpace('Sainsbury\'s Apricot Ripe & Ready x5');
+      expect(cleaned).to.be('Sainsbury\'s Apricot Ripe & Ready x5');
+    });
+
+    it('should be idempotent', function () {
+      var cleaned = transform.trimWhiteSpace('      Sainsbury\'s Apricot Ripe & Ready x5   ');
+      cleaned = transform.trimWhiteSpace(cleaned);
+      cleaned = transform.trimWhiteSpace(cleaned);
+      cleaned = transform.trimWhiteSpace(cleaned);
+      expect(cleaned).to.be('Sainsbury\'s Apricot Ripe & Ready x5');
+    });
+  });
+
+  describe('sumarise', function () {});
+
+  describe('formatAsKilloBytes', function () {});
+
+});
